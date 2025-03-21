4709235c1c2feeb3d5c6852a209ffcb83f0d0e9b
Sauravwastaken <sauravthecoder@gmail.com>
2025-03-22 00:22:26 +0530

    Initial project setup with basic sections

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php"],
+  theme: {
+    extend: {
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+      colors: {
+        theme_bg_light_yellow: "#FFFDF6",
+        theme_gray: "#EEF0F0",
+        theme_dim_gray: "#5D5F5F",
+        theme_green: "#225E56",
+      },
+    },
+  },
+  plugins: [],
+};
